f44151daf77b8589df1909ec774855bcf26dd643
1994wjdwodbs <16jwodbs@naver.com>
2021-08-02 09:40:09 +0900

    smartfarm 210802_0940

--- Flask/app.py
+++ Flask/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
- 
+
+# (데코레이터) '/' 경로
 @app.route("/")
-def home():
+def index():
     ip_address = request.remote_addr
     print("Requester IP: " + ip_address)
-    return "Hi"
+    # static/dummy.html 렌더링
+    return render_template("dummy.html")
  
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, threaded=True)

f05968f2a310e30bef0a04e0b324a72e11d771b9
1994wjdwodbs <16jwodbs@naver.com>
2021-08-02 11:07:16 +0900

    smartfarm 210802_1107

--- Flask/app.py
+++ Flask/app.py
@@ -6,31 +6,43 @@ app = Flask(__name__)
 def index():
     ip_address = request.remote_addr
     print("<Requester IP : " + ip_address + " >")
-    print(request.data)
+    print(request.get_data)
     # static/dummy.html 렌더링
     return render_template("dummy.html")
  
 @app.route("/login")
 def login():
-    
+    ip_address = request.remote_addr
+    print("<Requester IP : " + ip_address + " >")
+    print("<Login.html 접근>")
+    print(request.get_data)
     # static/Login.html 렌더링
     return render_template("Login.html")
 
 @app.route("/sign_up")
 def sign_up():
-
+    ip_address = request.remote_addr
+    print("<Requester IP : " + ip_address + " >")
+    print("<sign_up.html 접근>")
+    print(request.get_data)
     # static/sign_up.html 렌더링
     return render_template("sign_up.html")
 
 @app.route("/user_setting")
 def user_setting():
-
+    ip_address = request.remote_addr
+    print("<Requester IP : " + ip_address + " >")
+    print("<user_setting.html 접근>")
+    print(request.get_data)
     # static/Usersetting.html 렌더링
     return render_template("Usersetting.html")
 
 @app.route("/control")
 def control():
-
+    ip_address = request.remote_addr
+    print("<Requester IP : " + ip_address + " >")
+    print("<Control.html 접근>")
+    print(request.get_data)
     # static/Control.html 렌더링
     return render_template("Control.html")
 

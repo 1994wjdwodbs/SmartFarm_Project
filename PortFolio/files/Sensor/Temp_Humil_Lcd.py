b23f6fcb574b3714ca5fb8f7a33317f90bcd7295
1994wjdwodbs <16jwodbs@naver.com>
2021-08-05 14:39:07 +0900

    smartfarm 210805 1438 온습도센서 및 LCD센서 데모코드 작성

---
+++ PortFolio/files/Sensor/Temp_Humil_Lcd.py
@@ -0,0 +1,32 @@
+import RPi.GPIO as GPIO
+import dht11
+import drivers
+import time
+
+# initialize GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+# initialize LCD
+display = drivers.Lcd()
+
+# read data using pin 21
+instance = dht11.DHT11(pin = 21)
+try:
+	while True:
+		result = instance.read()
+		time.sleep(1)
+		
+		if result.is_valid():
+			print("Temp : %-3.1f C" % result.temperature)
+			print("Hum  : %-3.1f %%" % result.humidity)
+			display.lcd_display_string("Temp : %-3.1f C" % result.temperature, 1)
+			display.lcd_display_string("Hum  : %-3.1f %%" % result.humidity, 2)
+		else:
+			print("Error: %d" % result.error_code)
+		
+except KeyboardInterrupt:
+    print("Cleaning up!!")
+    display.lcd_clear()
+    GPIO.cleanup()

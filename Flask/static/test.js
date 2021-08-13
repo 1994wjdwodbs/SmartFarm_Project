12d99222bcbb2c55d859a539ca56d3864fcf7185
WhiteHair-H <pddana@naver.com>
2021-08-13 15:45:56 +0900

    컨트롤 화면 수정(조명, Pump)

--- Flask/static/test.js
+++ Flask/static/test.js
@@ -52,22 +52,27 @@ $(document).ready(function() {
         });
     });
     $('#switch4').change(function() {
-        $.ajax({
-          type:'POST',
-          dataType:'JSON',
-          url:'Choco',
-          data:{"Name" : "coco"},
-          success : function(data) {
-             if ($("#switch4").is(":checked")){
-                alert("On")
-             }
-             else
-             {
-                alert("Off")
-             }
-            },
-        });
-    });
+      $.ajax({
+        type:'POST',
+        dataType:'JSON',
+        url:'Choco',
+        data:{"Name" : "coco"},
+        success : function(data) {
+           if ($("#switch4").is(":checked")){
+              alert("On")
+           }
+           else
+           {
+              alert("Off")
+           }
+          },
+      });
+  });
+    $(function (){ $("#pressMe").one("click", function(){
+        alert("측정 시작"); 
+      }); 
+   });
+
    //  LED Range 함수
     $('.input-range').on('input', function () {
       $(this).next('.range-value').html(this.value);
